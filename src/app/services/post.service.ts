cb292a64f3be9b7726a2d27c03f4bde6c1f357f3
mastik14 <www.sergeypygach@mail.ru>
2023-06-24 19:57:41 +0300

    lesson 5 Get Categories using Higher Order Mapping Operators using mergeMap

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost } from '../models/IPost';
-import { Subscription, map } from 'rxjs';
+import { Subscription, map, mergeMap } from 'rxjs';
+import { CategoryService } from './category.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,10 @@ import { Subscription, map } from 'rxjs';
 export class PostService {
   posts: IPost[] = [];
   postsSubscription!: Subscription;
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService
+  ) {}
 
   getPosts() {
     return this.http.get<{ [id: string]: IPost }>(
@@ -23,5 +27,24 @@ export class PostService {
 
       return postData;
     }));
+  }
+
+  getPostsWithCategory() {
+    return this.getPosts().pipe(
+      mergeMap((posts) => {
+        return this.categoryService.getCategories().pipe(
+          map((categories) => {
+            return posts.map((post) => {
+              return {
+                ...post,
+                categoryName: categories.find(
+                  (category) => category.id === post.categoryId
+                )?.title,
+              };
+            });
+          })
+        );
+      })
+    );
   }
 }
