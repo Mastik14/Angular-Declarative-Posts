4ce00925f2f2ca2d85f19810adb06573985ae84c
mastik14 <www.sergeypygach@mail.ru>
2023-06-24 19:36:06 +0300

    lesson 4 format posts response using rxjs map operator and display in the template using ngFor

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,16 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost } from '../models/IPost';
+import { Subscription, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
+  posts: IPost[] = [];
+  postsSubscription!: Subscription;
   constructor(private http: HttpClient) {}
 
   getPosts() {
     return this.http.get<{ [id: string]: IPost }>(
       `https://rxjs-posts-default-rtdb.firebaseio.com/posts.json`
-    );
+    ).pipe(map((posts) => {
+      let postData: IPost[] = [];
+
+      for (let id in posts) {
+        postData.push({...posts[id], id});
+      }
+
+      return postData;
+    }));
   }
 }
