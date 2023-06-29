5dd1e4b404178e0046cf23cfaab62dd961b64d89
mastik14 <www.sergeypygach@mail.ru>
2023-06-29 21:51:29 +0300

    lesson 15 Show Error Messages using the catchError Operator and assign to the error Subject

--- src/app/components/single-post/single-post.component.ts
+++ src/app/components/single-post/single-post.component.ts
@@ -1,15 +1,22 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, catchError, EMPTY } from 'rxjs';
 import { DeclarativePostService } from 'src/app/services/DeclarativePost.service';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
   styleUrls: ['./single-post.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SinglePostComponent implements OnInit {
-  post$ = this.postService.post$;
-  constructor(private postService: DeclarativePostService) {}
+export class SinglePostComponent {
+  errorMessageSubject = new BehaviorSubject<string>('');
+  errorMessageAction$ = this.errorMessageSubject.asObservable();
 
-  ngOnInit(): void {}
+  post$ = this.postService.post$.pipe(
+    catchError((error: string) => {
+      this.errorMessageSubject.next(error);
+      return EMPTY;
+    })
+  );
+  constructor(private postService: DeclarativePostService) {}
 }
