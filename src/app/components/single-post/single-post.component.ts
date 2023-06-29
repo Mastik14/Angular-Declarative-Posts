5ab6acf8cb341938c251106a80f787d66eff61eb
mastik14 <www.sergeypygach@mail.ru>
2023-06-29 21:32:27 +0300

    lesson 14 Create Action Stream using Subject for getting Post Details between components

--- src/app/components/single-post/single-post.component.ts
+++ src/app/components/single-post/single-post.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { DeclarativePostService } from 'src/app/services/DeclarativePost.service';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.scss']
+  styleUrls: ['./single-post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SinglePostComponent {
+export class SinglePostComponent implements OnInit {
+  post$ = this.postService.post$;
+  constructor(private postService: DeclarativePostService) {}
 
+  ngOnInit(): void {}
 }
